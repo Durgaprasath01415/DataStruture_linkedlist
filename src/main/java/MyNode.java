dbcc2789689c54cca8be623f3d6f97c730f2ebc3
Durgaprasath01415 <83449179+Durgaprasath01415@users.noreply.github.com>
2021-06-08 23:35:17 +0530

    ADDED : Created a simple linked list

--- src/main/java/MyNode.java
+++ src/main/java/MyNode.java
@@ -1,2 +1,18 @@
-public class MyNode {
+public class MyNode<T> {
+
+    private T key;
+    private MyNode<T> next;
+
+    public MyNode (T key) {
+        this.key = key;
+        this.next = null;
+    }
+
+    public MyNode<T> getNext() {
+        return next;
+    }
+
+    public void setNext(MyNode<T> next) {
+        this.next = next;
+    }
 }
