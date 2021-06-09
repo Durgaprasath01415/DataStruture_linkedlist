ba120b6fe2ac7e2a9eb9ef62b2ed2932936580fd
Durgaprasath01415 <83449179+Durgaprasath01415@users.noreply.github.com>
2021-06-09 06:59:29 +0530

    ADDED : Functionality to pop the first element from linkedlist

--- src/test/java/LinkedListTest.java
+++ src/test/java/LinkedListTest.java
@@ -60,4 +60,24 @@ public class LinkedListTest {
                 myLinkedList.tail.equals(myThirdNode);
         Assert.assertTrue(result);
     }
+
+    @Test
+    public void givenFirstElement_WhenDeleted_ShouldPassLinkedListResult() {
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+
+        LinkedList<Integer> myLinkedList = new LinkedList<>();
+
+        myLinkedList.add(myFirstNode);
+        myLinkedList.append(mySecondNode);
+        myLinkedList.append(myThirdNode);
+        myLinkedList.pop();
+
+        myLinkedList.printNodes();
+
+        boolean result = myLinkedList.head.equals(mySecondNode) &&
+                myLinkedList.tail.equals(myThirdNode);
+        Assert.assertTrue(result);
+    }
 }
